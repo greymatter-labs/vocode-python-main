dc724943daf0707f41c498676c093e2b0ec122a4
Arthur S <silversteinarthur@gmail.com>
2024-03-20 23:47:07 -0700

    short circuit voice id

--- vocode/streaming/synthesizer/azure_synthesizer.py
+++ vocode/streaming/synthesizer/azure_synthesizer.py
@@ -235,6 +235,15 @@ class AzureSynthesizer(BaseSynthesizer[AzureSynthesizerConfig]):
         volume: int = 1,
         rate: int = 1,
     ) -> str:
+        if self.synthesizer_config.azure_speaker_id != 'none':
+            ssml_root = ElementTree.fromstring(
+                f'<speak version="1.0" xmlns="http://www.w3.org/2001/10/synthesis" xmlns:mstts="http://www.w3.org/2001/mstts" xml:lang="en-US">'
+            )
+            voice = ElementTree.SubElement(ssml_root, "voice", {"name": "DragonLatestNeural"})
+            tts_embedding = ElementTree.SubElement(ssml_root, "mstts:ttsembedding", {"speakerProfileId": self.synthesizer_config.azure_speaker_id})
+            tts_embedding.text = message
+            return ElementTree.tostring(ssml_root, encoding="unicode")
+
         voice_language_code = self.synthesizer_config.language_code
         if voice_language_code != "en-US" and voice_language_code != "en":
             rate = 0.1
@@ -246,11 +255,7 @@ class AzureSynthesizer(BaseSynthesizer[AzureSynthesizerConfig]):
         )
         voice = ElementTree.SubElement(ssml_root, "voice")
         voice.set("name", self.voice_name)
-        if self.synthesizer_config.azure_speaker_id != 'none':
-            voice.set("name", "DragonLatestNeural")
-            tts_embedding = ElementTree.SubElement(ssml_root, "mstts:ttsembedding", {"speakerProfileId": self.synthesizer_config.azure_speaker_id})
-            voice_root = tts_embedding
-        elif self.synthesizer_config.language_code != "en-US":
+        if self.synthesizer_config.language_code != "en-US":
             lang = ElementTree.SubElement(voice, "{%s}lang" % NAMESPACES.get(""))
             lang.set("xml:lang", self.synthesizer_config.language_code)
             voice_root = lang
