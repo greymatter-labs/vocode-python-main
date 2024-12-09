68b93e71759860f46ac64d2954cad3004a068cda
chun1 <arthur@chunai.dev>
2024-12-09 19:54:20 +1100

    convert output buffer to audio pcm

--- vocode/streaming/output_device/websocket_output_device.py
+++ vocode/streaming/output_device/websocket_output_device.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import asyncio
 from fastapi import WebSocket
+import numpy as np
 from vocode.streaming.models.audio_encoding import AudioEncoding
 from vocode.streaming.output_device.base_output_device import BaseOutputDevice
 from vocode.streaming.models.websocket import AudioMessage
@@ -9,15 +10,26 @@ from vocode.streaming.models.websocket import TranscriptMessage
 from vocode.streaming.models.transcript import TranscriptEvent
 
 
+def convert_linear16_to_pcm(linear16_audio: bytes) -> bytes:
+    audio_array = (
+        np.frombuffer(linear16_audio, dtype=np.int16).astype(np.float32) / 32768.0
+    )
+    return audio_array.tobytes()
+
 
 class WebsocketOutputDevice(BaseOutputDevice):
     def __init__(
-        self, ws: WebSocket, sampling_rate: int, audio_encoding: AudioEncoding
+        self,
+        ws: WebSocket,
+        sampling_rate: int,
+        audio_encoding: AudioEncoding,
+        into_pcm: bool = False,
     ):
         super().__init__(sampling_rate, audio_encoding)
         self.ws = ws
         self.active = False
         self.queue: asyncio.Queue[str] = asyncio.Queue()
+        self.into_pcm = into_pcm
 
     def start(self):
         self.active = True
@@ -33,7 +45,27 @@ class WebsocketOutputDevice(BaseOutputDevice):
 
     def consume_nonblocking(self, chunk: bytes):
         if self.active:
+            if self.into_pcm:
+                if self.audio_encoding == AudioEncoding.LINEAR16:
+                    chunk = convert_linear16_to_pcm(chunk)
+                elif self.audio_encoding == AudioEncoding.MULAW:
+                    raise ValueError("Mu-law encoding is not supported yet")
+                else:
+                    raise ValueError(
+                        f"Unsupported audio encoding: {self.audio_encoding}"
+                    )
             audio_message = AudioMessage.from_bytes(chunk)
+            # if self.into_pcm:
+            # if audio_message.audio_encoding == AudioEncoding.LINEAR16:
+            #     audio_message.data = convert_linear16_to_pcm(audio_message.data)
+            # elif audio_message.audio_encoding == AudioEncoding.MULAW:
+            #     raise ValueError("Mu-law encoding is not supported yet")
+            #     audio_message.data = mulaw_to_pcm(audio_message.data)
+            # else:
+            #     raise ValueError(
+            #         f"Unsupported audio encoding: {audio_message.audio_encoding}"
+            # )
+
             self.queue.put_nowait(audio_message.json())
 
     def consume_transcript(self, event: TranscriptEvent):
